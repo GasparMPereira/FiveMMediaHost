ac3d345f88ef47ce11155b245c2ac8a2ea0a5c9e
Gaspar <71574610+GasparMPereira@users.noreply.github.com>
2024-05-13 16:02:56 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,2 +1,3 @@
 Config = {}
-Config.IDLength = 30
+Config.ServerHost = GetConvar("web_baseUrl", "") --// Use CFX.re domain of your server OR you can replace with your custom domain name
+Config.IDLength = 30 --// Image name length
