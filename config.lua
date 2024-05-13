7c1bf6493c614d821ef14a8c0f34d56c2e7cb719
Gaspar <71574610+GasparMPereira@users.noreply.github.com>
2024-05-13 16:36:22 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,4 @@
 Config = {}
-Config.ServerHost = GetConvar("web_baseUrl", "") --// Use CFX.re domain of your server OR you can replace with your custom domain name
+Config.ServerHost = "https://"..GetConvar("web_baseUrl", "").."/"..GetCurrentResourceName() --// Use CFX.re domain of your server
+--Config.ServerHost = "http://127.0.0.1:30120/"..GetCurrentResourceName() --// OR you can replace with your custom domain name/IP address
 Config.IDLength = 30 --// Image name length
