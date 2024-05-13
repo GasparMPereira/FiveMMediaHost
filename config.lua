fddca6ed6cc77a4ae0670bc88d9c65afaf130aac
Gaspar <71574610+GasparMPereira@users.noreply.github.com>
2024-05-14 00:57:50 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,3 +2,4 @@ Config = {}
 Config.ServerHost = "https://"..GetConvar("web_baseUrl", "").."/"..GetCurrentResourceName() --// Use CFX.re domain of your server
 --Config.ServerHost = "http://127.0.0.1:30120/"..GetCurrentResourceName() --// OR you can replace with your custom domain name/IP address
 Config.IDLength = 30 --// Image name length
+Config.ImageDirectory = "resources/[phone]/"..GetCurrentResourceName().."/images/"
