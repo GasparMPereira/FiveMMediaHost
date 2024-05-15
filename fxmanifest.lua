d4ecd6431a26d2fbdd48bad65f9c0cc87d4c9e16
Gaspar <71574610+GasparMPereira@users.noreply.github.com>
2024-05-15 20:30:03 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 game 'gta5'
 author 'Gaspar Pereira - Discord: gasparmpereira'
 description 'Image Hosting Resource'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts{
     'config.lua'
