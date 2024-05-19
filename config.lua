e3a111f6e7d7a1acd6d0946245d27ab8856e376a
Gaspar <71574610+GasparMPereira@users.noreply.github.com>
2024-05-19 01:31:11 +0100

    Update config.lua
    Added support for audio and video

--- config.lua
+++ config.lua
@@ -2,4 +2,7 @@ Config = {}
 Config.UseCFXReDomain = true --// Use CFX.re domain of your server? TRUE RECOMMENDED
 Config.ServerHost = "http://127.0.0.1:30120/"..GetCurrentResourceName() --// OR you can replace with your custom domain name/IP address
 Config.IDLength = 30 --// Image name length
+Config.MediaEndpoint = 'media' --//
 Config.ImageDirectory = "resources/"..GetCurrentResourceName().."/images/" --//
+Config.SoundDirectory = "resources/"..GetCurrentResourceName().."/sounds/" --//
+Config.VideoDirectory = "resources/"..GetCurrentResourceName().."/videos/" --//
